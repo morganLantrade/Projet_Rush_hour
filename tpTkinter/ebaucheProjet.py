c2080d3f2fc35f4371ba1c30378ce750ba2d2cd1
morganLantrade <74090897+morganLantrade@users.noreply.github.com>
2021-01-08 16:23:20 +0100

    Update ebaucheProjet.py

--- tpTkinter/ebaucheProjet.py
+++ tpTkinter/ebaucheProjet.py
@@ -9,20 +9,15 @@ COLORS=["white",'red',"yellow","blue","green","orange","purple","grey"]
 Grid= [ [0,0,2,0,0,0],[3,0,2,0,3,3],[3,0,2,0,0,0],[0,1,1,0,4,5],[6,6,0,0,4,5],[3,3,3,0,7,7] ]
 
 class vehicule:
+	ID=0
 	def __init__(self,listeCoord,color):
+		self.id=ID
+		ID+=1
 		self.coords=listeCoord
 		self.color=color
 		self.direction=(1,0) if listeCoord[0][0]==listeCoord[1][0] else  (0,1)
 		self.lg=len(listeCoord)
-		if self.direction=(1,0):
-			if self.coords[0][1]>0:
-				A=(listeCoord[0][1]-1,listeCoord[0][0]) else (-1,-1)
-			if self.coords[self.lg-1][1]<5:
-				B=(listeCoord[self.lg-1][1]+1,listeCoord[self.lg-1])
-			self.casePossibles=[]
-			if (Grid[A[0]][A[1]]==0) self.casePossibles.append(A)
-			if (Grid[B[0]][B[1]]==0) self.casePossibles.append(B)
-
+		
 def updateGrid(vehicule):
 	for (x,y) in vehicule.listeCoord:
 		if Grid[x][y]!=0 : return;
