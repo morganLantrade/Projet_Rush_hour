c57cad4a39b3bd9576521cb3619d29f52d6eb391
Gowthraven <62013075+Gowthraven@users.noreply.github.com>
2021-04-14 06:48:09 +0200

    Add files via upload

--- Menu2test.py
+++ Menu2test.py
@@ -1,15 +1,34 @@
 from tkinter import * 
+import pygame
 from PIL import Image, ImageTk
 
+pygame.mixer.init()
+pygame.mixer.music.load("musique.mp3") 
+
+def lancer_musique():
+	pygame.mixer.music.set_volume(0.5)
+	pygame.mixer.music.play(-1)
+
+def arret_son():
+	pygame.mixer.music.stop()
+
+def quitter():
+	pygame.quit()
+	fenetre.destroy()
 
 def show_frame(frame):
     frame.tkraise()
     
 fenetre = Tk()
-fenetre.state('zoomed')
+
+
+fenetre.geometry("800x600")
 fenetre.title("CANDY RUSH HOUR")
 
-image = Image.open(r"C:\Users\Utilisateur\Desktop\PROJET PYTHON 2021\oui.jpg")
+fenetre.protocol("WM_DELETE_WINDOW",quitter)
+
+#Ouverture d'image pour la 
+image = Image.open(r"oui.jpg")
 photo = ImageTk.PhotoImage(image.resize((196, 196), Image.ANTIALIAS))
 
 fenetre.rowconfigure(0, weight=1)
@@ -26,6 +45,13 @@ frame1_title.pack(fill='both', expand=True)
 
 frame1_btn = Button(frame1, text='Enter',command=lambda:show_frame(frame2))
 frame1_btn.pack(fill='x', ipady=16)
+
+son1 = Image.open(r"son_on.jpg")
+image_son1 = ImageTk.PhotoImage(son1.resize((65, 65), Image.ANTIALIAS))
+
+Button(frame1,image=image_son1,command=lancer_musique,bg="white").pack(pady=10)
+
+Button(frame1,text="Couper",command=arret_son,width=40).pack(pady=10)
 
 #==================Menu 2 : Les règles 
 #frame2_title=  Label(frame2, image=test, bg='yellow')
